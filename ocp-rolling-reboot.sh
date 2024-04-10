b438564ca3ca5c713cdb372670f68832e1dcef48
root <tmcginni@redhat.com>
2024-04-10 14:03:56 -0400

    Add restart capabilities

--- ocp-rolling-reboot.sh
+++ ocp-rolling-reboot.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-# Perform rolling reboot of Openshift nodes
+# Perform rolling reboot of OCP nodes
 #
-# Uses the node_order.sh utility to compute best order to reboot the nodes
+# Note: This script uses a smart node order script to reboot with the least amount of impact on the cluster.
 #
-# T.McGinnis
+# T.McGinnis 12/2023
 #
 #set -x
-
+ 
 initial_wait_interval="45"
-initial_wait_interval="1"
-
+ping_after_reboot_threshold="120"       # Number of seconds to wait for a ping after rebooting the node.  Alert when this is exceeded.
+ 
 SCRIPT_DIR="$(dirname $0)"
-
+ 
+if [ "$1" = "-l" ]; then
+   RESTART_AT_NODE="$2"
+   if [ "$RESTART_AT_NODE" != "" ]; then
+      RESTART="Y"
+      echo "Restarting after node \"$RESTART_AT_NODE"
+   fi
+fi
+ 
 oc get nodes >/dev/null
 if [ $? -ne 0 ]; then
    echo "You must be logged in to perform this action"
    exit 1
 fi
-
+ 
 echo ""
 echo "About to reboot nodes for this cluster  >>> $(oc whoami --show-server)"
 echo ""
@@ -26,24 +34,49 @@ if [ "${ans^^}" != "Y" ]; then
    echo "Reboots aborted."
    exit 2
 fi
-
+ 
 function waitforready {
 while [ "$(oc get node $nodename --no-headers=true|grep -v " Ready ")" != "" ]
 do
    oc get node $nodename
    sleep 5
+   ping -c 1 $nodename >/dev/null 2>&1
+   if [ $? -ne 0 ]; then
+      CURRENT_TIME=`date +%s`
+      let ping_delay=CURRENT_TIME-SHUTDOWN_TIME
+      if [ $ping_delay -ge $ping_after_reboot_threshold ]; then
+         echo "WARNING: This node is still not reachable after $ping_delay seconds.  Might need to investigate why."
+      fi
+   fi
 done
 }
-
+ 
 NODELIST="$($SCRIPT_DIR/node_order.sh|awk '{print $2}')"
 echo "NODELIST:$NODELIST"
 #for nodename in $(oc get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="Hostname")].address}')
 for nodename in $NODELIST
 do
-   echo "reboot node $nodename"
-   echo "ssh -o StrictHostKeyChecking=no core@$nodename sudo shutdown -r -t 3"
+   if [ "$RESTART" = "Y" ]; then
+      if [ "$nodename" = "$RESTART_AT_NODE" ]; then
+         RESTART="N"
+      fi
+      continue
+   fi
+   echo "rebooting node $nodename"
+   #sudo ssh -o StrictHostKeyChecking=no core@$nodename sudo shutdown -r 0
+   if [ $? -ne 0 ]; then
+      echo ""
+      echo "ERROR!!!!!!    Failed to initiate node shutdown for $nodename."
+      echo ""
+      read -p "  Press \"Y\" to continue or ctrl-c to abort: " ans
+      if [ "${ans^^}" != "Y" ]; then
+         echo "Reboots aborted."
+         exit 3
+      fi
+   fi
+   SHUTDOWN_TIME=`date +%s`
    oc get node $nodename
    sleep $initial_wait_interval
    waitforready
+   oc get node $nodename
 done
-
